17e113fb82acf0901b28cd26702415b66c532675
kkskyw <lywww923@gmail.com>
2025-11-18 01:09:22 +0800

    front end for donation page

---
+++ public/script/donation.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const amountButtons = document.querySelectorAll(".amount-button");
+    const customAmountInput = document.getElementById("customAmount");
+    const donationForm = document.getElementById("donationForm");
+});
