92b8d4cf2e27bb515f02838bbac3037c1682c145
YaAn-nnn <yaan1314@gmail.com>
2026-01-30 13:33:22 +0800

    fix file name

--- public/script/viewEvent.js
+++ public/script/viewEvent.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const backBtn = document.getElementById('backBtn');
     if (backBtn) {
         backBtn.addEventListener('click', function() {
-            window.location.href = 'main.html';
+            window.location.href = 'volunteer_main.html';
         });
     }
     
